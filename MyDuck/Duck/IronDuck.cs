4b3bf694e5c55a625f8c052d4480b4a22955866c
Константин Орловский <kos.orlovsky@gmail.com>
2020-10-24 23:40:44 +0300

    feat: Add iron duck

---
+++ MyDuck/Duck/IronDuck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDuck.Duck
+{
+    class IronDuck:BaseDuck
+    {
+        public override void Display() => Console.WriteLine("Simple duck was created!!!");
+    }
+}
